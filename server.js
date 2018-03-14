7fa1f711d8bed392683dae3d12badbe4d045f0b3
David Nyman <davenyman@gmail.com>
2018-03-14 15:07:57 -0400

    Change hello world in new route
    This is fun

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+app.get('/new', (req, res) => {
+  res.send('Hello WORLD');
+});
+app.get('/another', (req, res) => {
+  res.send('WHAT THE HELL, THIS IS COOL');
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Running on ${PORT}`);
+});
