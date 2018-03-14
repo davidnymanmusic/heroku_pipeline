1c41a3f9c5faa12719c4bf604821e227c802b1d4
David Nyman <davenyman@gmail.com>
2018-03-14 14:57:33 -0400

    Add new route
    The route says "HEY WORLD"

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+app.get('/new', (req, res) => {
+  res.send('HEY WORLD');
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
